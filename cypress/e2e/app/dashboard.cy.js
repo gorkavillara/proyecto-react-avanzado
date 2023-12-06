2241076fd36353529b2fbbd65f31345ff8a9e42c
gorkavillara <gorkavillara@gmail.com>
2023-12-06 23:10:04 +0100

    Cypress

--- cypress/e2e/app/dashboard.cy.js
+++ cypress/e2e/app/dashboard.cy.js
@@ -1,29 +1,7 @@
 /// <reference types="cypress" />
 
 describe("Pantalla de tareas", () => {
-    beforeEach(() => {
-        // cy.visit("http://localhost:3000")
+    it("Renderiza la página correctamente", () => {
         cy.visit("https://proyecto-react-avanzado-nu.vercel.app/")
-    })
-
-    it("Muestra once elementos por defecto", () => {
-        cy.get("#tasks-table tbody tr").should("have.length", 11)
-
-        cy.get("#tasks-table tbody tr")
-            .last()
-            .find("td")
-            .first()
-            .should("have.text", "Diseñar la página de configuración")
-    })
-
-    it("Puede añadir tareas nuevas", () => {
-        const nuevaTarea = "Crear los tests de Cypress"
-
-        cy.get("#tasks-table tbody tr td input").type(nuevaTarea)
-
-        cy.get("#user-select").select("anaperez")
-
-        cy.get("#create-task-button").click()
-        cy.get("#tasks-table tbody tr").should("have.length", 12)
     })
 })
